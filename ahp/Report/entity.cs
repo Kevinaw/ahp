ebaed9fbaba91a43764986dd5c404807066d5056
Kevinaw <Kevinaw613@gmail.com>
2017-09-22 12:19:17 -0600

    Release on 2017-09-22,

---
+++ ahp/Report/entity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ahp.Report
+{
+    class entity
+    {
+        public string goal { get; set; }
+
+        public List<StrctCriteria> listCr { get; set; }
+        double[,] mtx { get; set; }
+
+        public List<StrctAlternative> listAl { get; set; }
+        int[,] mtxAC { get; set; }
+    }
+}
